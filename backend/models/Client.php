5f1fac89142cee244765b7ecce528fb5944d138e
MoroesiR <mavundlamoroesi@gmail.com>
2026-01-15 11:16:36 +0200

    feat: add Client model

---
+++ backend/models/Client.php
@@ -0,0 +1,38 @@
+<?php
+require_once __DIR__ . '/../config/db.php';
+
+class Client {
+    private $conn;
+    private $table = "clients";
+
+    public $id;
+    public $name;
+    public $email;
+    public $loan_amount;
+    public $created_at;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->connect();
+    }
+
+    // Create new client
+    public function create() {
+        $query = "INSERT INTO " . $this->table . " (name, email, loan_amount) 
+                  VALUES (:name, :email, :loan_amount)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":loan_amount", $this->loan_amount);
+        return $stmt->execute();
+    }
+
+    // Read all clients
+    public function readAll() {
+        $query = "SELECT * FROM " . $this->table . " ORDER BY created_at DESC";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+}
+?>
